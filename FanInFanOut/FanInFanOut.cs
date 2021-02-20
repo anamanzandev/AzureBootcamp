cc76fdc48d73bcf060c5ba88c686df349b895387
Ana Manzan <ana.manzan@gmail.com>
2021-02-20 16:40:08 -0300

    Fan in/fan out implementation

--- FanInFanOut/FanInFanOut.cs
+++ FanInFanOut/FanInFanOut.cs
@@ -1,10 +1,10 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 
 namespace Bootcamp
@@ -15,22 +15,36 @@ namespace Bootcamp
         public static async Task<List<string>> RunOrchestrator(
             [OrchestrationTrigger] IDurableOrchestrationContext context)
         {
-            var outputs = new List<string>();
+            var tasks = new List<Task<string>>();
 
-            // Replace "hello" with the name of your Durable Activity Function.
-            outputs.Add(await context.CallActivityAsync<string>("FanInFanOut_Hello", "Tokyo"));
-            outputs.Add(await context.CallActivityAsync<string>("FanInFanOut_Hello", "Seattle"));
-            outputs.Add(await context.CallActivityAsync<string>("FanInFanOut_Hello", "London"));
+            var sales = await context.CallActivityAsync<List<int>>("FetchPaidSales", null);
 
-            // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
-            return outputs;
+            foreach (var sale in sales)
+            {
+                tasks.Add(context.CallActivityAsync<string>("ProcessShippingInformation", sale));
+            }
+
+            await Task.WhenAll(tasks);
+
+            return tasks.Select(t => t.Result).ToList();
         }
 
-        [FunctionName("FanInFanOut_Hello")]
-        public static string SayHello([ActivityTrigger] string name, ILogger log)
+        [FunctionName("FetchPaidSales")]
+        public static List<int> FetchPaidSales([ActivityTrigger] string name, ILogger log)
         {
-            log.LogInformation($"Saying hello to {name}.");
-            return $"Hello {name}!";
+            log.LogInformation("Fetching not sent paid sales");
+            // Your DB Code here
+            return new List<int>() { 10, 11, 15, 17, 12, 16, 20 };
+        }
+
+        [FunctionName("ProcessShippingInformation")]
+        public static string ProcessShippingInformation([ActivityTrigger] int idSale, ILogger log)
+        {
+            log.LogInformation($"Processing shipping information for sale {idSale}");
+
+            // Insert here your logic 
+
+            return $"Processed shipping information for sale {idSale}";
         }
 
         [FunctionName("FanInFanOut_HttpStart")]
